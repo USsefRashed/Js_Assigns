5c0f4b6ed2c4e79b440cd6e7ecd80910741898e8
USsefRashed <USsefRashed@gmail.com>
2022-02-08 09:57:16 -0500

    element Attributes with js

---
+++ 02/main.js
@@ -0,0 +1,5 @@
+document.getElementById("el0").style.color="Blue";
+document.getElementById("el0").style.fontWeight="Bold";
+document.getElementById("el0").style.fontSize="80px";
+document.getElementById("el0").style.textAlign="center";
+document.getElementById("el0").style.fontFamily="Arial"
